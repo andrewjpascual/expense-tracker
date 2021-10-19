68cc1f49ed00cb059d0bddb7cc56d9b39c08dfcf
andrewjpascual <57194224+andrewjpascual@users.noreply.github.com>
2021-10-18 22:15:17 -0700

    Update DoughnutChart.js

--- src/components/DoughnutChart.js
+++ src/components/DoughnutChart.js
@@ -6,65 +6,162 @@ import { GlobalContext } from "../context/GlobalState";
 const DoughnutChart = () => {
   const { transactions } = useContext(GlobalContext);
 
-  // Map the transaction categories into an array
-  //   const categoriess = transactions.map((transaction) => transaction.category);
-
-  // Testing data
-  //   const grouped = transactions.reduce(
-  //     (transactionItem, { category, title }) => {
-  //       if (!transactionItem[category]) transactionItem[category] = [];
-  //       transactionItem[category].push(title);
-  //       return transactionItem;
-  //     },
-  //     {}
-  //   );
-
   //set of unique categories
   const categorySet = new Set(
     transactions.map((transaction) => transaction.category)
   );
 
-  // sort the set
-  const categories = Array.from(categorySet);
+  // Sort the set
+  const categories = Array.from(categorySet).sort();
 
-  // make an array for each category
-  let fake = transactions;
-  fake = transactions.filter((transaction) => transaction.category == "home");
-  console.log(fake);
+  // Now make an array for each category
 
-  const homeTotal = fake.reduce(
+  // All food payments and total amount
+  let foodM = transactions;
+  foodM = transactions.filter((transaction) => transaction.category === "food");
+
+  const foodTotal = foodM.reduce(
+    (totalAmount, food) => totalAmount + food.amount,
+    0
+  );
+
+  // All home payments and total amount
+  let homeM = transactions;
+  homeM = transactions.filter((transaction) => transaction.category === "home");
+  console.log(homeM);
+
+  const homeTotal = homeM.reduce(
     (totalAmount, home) => totalAmount + home.amount,
     0
   );
   console.log(homeTotal);
 
-  // get the total for each category
+  // All bill payments and total amount
+  let billM = transactions;
+  billM = transactions.filter(
+    (transaction) => transaction.category === "bills"
+  );
+
+  const billsTotal = billM.reduce(
+    (totalAmount, bills) => totalAmount + bills.amount,
+    0
+  );
+
+  // All groceries payments and total amount
+  let groceriesM = transactions;
+  groceriesM = transactions.filter(
+    (transaction) => transaction.category === "groceries"
+  );
+
+  const groceriesTotal = groceriesM.reduce(
+    (totalAmount, groceries) => totalAmount + groceries.amount,
+    0
+  );
+
+  // All technology payments and total amount
+  let technologyM = transactions;
+  technologyM = transactions.filter(
+    (transaction) => transaction.category === "technology"
+  );
+
+  const technologyTotal = technologyM.reduce(
+    (totalAmount, technology) => totalAmount + technology.amount,
+    0
+  );
+
+  // All health payments and total amount
+  let healthM = transactions;
+  healthM = transactions.filter(
+    (transaction) => transaction.category === "health"
+  );
+
+  const healthTotal = healthM.reduce(
+    (totalAmount, health) => totalAmount + health.amount,
+    0
+  );
+
+  // All entertainment payments and total amount
+  let entertainmentM = transactions;
+  entertainmentM = transactions.filter(
+    (transaction) => transaction.category === "entertainment"
+  );
+
+  const entertainmentTotal = entertainmentM.reduce(
+    (totalAmount, entertainment) => totalAmount + entertainment.amount,
+    0
+  );
+
+  // All travel payments and total amount
+  let travelM = transactions;
+  travelM = transactions.filter(
+    (transaction) => transaction.category === "travel"
+  );
+
+  const travelTotal = travelM.reduce(
+    (totalAmount, travel) => totalAmount + travel.amount,
+    0
+  );
+
+  // All pets payments and total amount
+  let petsM = transactions;
+  petsM = transactions.filter((transaction) => transaction.category === "pets");
+
+  const petsTotal = petsM.reduce(
+    (totalAmount, pets) => totalAmount + pets.amount,
+    0
+  );
 
   console.log(transactions);
 
   console.log(categories);
 
   const data = {
-    labels: categories,
+    labels: [
+      "Food",
+      "Home",
+      "Bills",
+      "Groceries",
+      "Technology",
+      "Health",
+      "Entertainment",
+      "Travel",
+      "Pets",
+    ],
     datasets: [
       {
         label: "Allocated funds",
-        data: [12, 19, 3, 5, 2, 3],
+        data: [
+          foodTotal,
+          homeTotal,
+          billsTotal,
+          groceriesTotal,
+          technologyTotal,
+          healthTotal,
+          entertainmentTotal,
+          travelTotal,
+          petsTotal,
+        ],
         backgroundColor: [
-          "rgba(255, 99, 132, 0.8)",
+          "rgba(255, 51, 0, 0.8)",
           "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
+          "rgba(255, 206, 86, 1)",
           "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
+          "rgba(0, 102, 255, 1)",
           "rgba(255, 159, 64, 0.2)",
+          "rgba(51, 204, 51, 0.3)",
+          "rgba(153, 102, 255, 0.4)",
+          "rgba(255, 102, 0, 0.4)",
         ],
         borderColor: [
           "rgba(255, 99, 132, 1)",
           "rgba(54, 162, 235, 1)",
           "rgba(255, 206, 86, 1)",
           "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
+          "rgba(0, 102, 255, 1)",
           "rgba(255, 159, 64, 1)",
+          "rgba(51, 204, 51, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 102, 0, 1)",
         ],
         borderWidth: 1,
       },
