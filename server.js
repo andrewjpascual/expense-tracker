80efc29dfa504c249387a5c24eb294c5b7289117
andrewjpascual <57194224+andrewjpascual@users.noreply.github.com>
2021-10-22 02:10:36 -0700

    Version 1.0.0

--- server.js
+++ server.js
@@ -15,26 +15,28 @@ const app = express();
 
 app.use(express.json());
 
+// serve a development view, with morgan to show routes hit
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 
 app.use("/api/v1/transactions", transactions);
 
+// serve the static assets build folder
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    );
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-    PORT,
-    console.log(
-        `[express] Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-            .magenta.bold
-    )
+  PORT,
+  console.log(
+    `[express] Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      .magenta.bold
+  )
 );
